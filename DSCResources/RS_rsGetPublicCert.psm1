a7147155bbc336ff70feec1a1fdfdb53f604e043
Mark Key <mark.key@rackspace.com>
2015-04-27 14:40:08 -0500

    init

---
+++ DSCResources/RS_rsGetPublicCert.psm1
@@ -0,0 +1,51 @@
+﻿Function Get-TargetResource {
+  param (
+    [parameter(Mandatory = $true)][string]$Name
+  )
+  $nodeinfo = Get-Content ([Environment]::GetEnvironmentVariable('nodeInfoPath','Machine').ToString()) -Raw | ConvertFrom-Json
+  $uri = "https://$($nodeinfo.PullServerIP):$($nodeinfo.PullServerPort)"
+  $webRequest = [Net.WebRequest]::Create($uri)
+  try { $webRequest.GetResponse() } catch {}
+  $cert = $webRequest.ServicePoint.Certificate
+  return @{
+    'Name' = $Name
+    'Result' = (Get-ChildItem Cert:\LocalMachine\Root | ? Thumbprint -eq ($cert.GetCertHashString()))
+  }
+}
+
+Function Test-TargetResource {
+  param (
+    [parameter(Mandatory = $true)][string]$Name
+  )
+  $nodeinfo = Get-Content ([Environment]::GetEnvironmentVariable('nodeInfoPath','Machine').ToString()) -Raw | ConvertFrom-Json
+  $uri = "https://$($nodeinfo.PullServerIP):$($nodeinfo.PullServerPort)"
+  $webRequest = [Net.WebRequest]::Create($uri)
+  try { $webRequest.GetResponse() } catch {}
+  $cert = $webRequest.ServicePoint.Certificate
+  if((Get-ChildItem Cert:\LocalMachine\Root).Thumbprint -contains ($cert.GetCertHashString())) {
+    return $true
+  }
+  else {
+    return $false
+  }
+
+}
+
+Function Set-TargetResource {
+  param (
+    [parameter(Mandatory = $true)][string]$Name
+  )
+  $nodeinfo = Get-Content ([Environment]::GetEnvironmentVariable('nodeInfoPath','Machine').ToString()) -Raw | ConvertFrom-Json
+  $uri = "https://$($nodeinfo.PullServerIP):$($nodeinfo.PullServerPort)"
+  $webRequest = [Net.WebRequest]::Create($uri)
+  try { $webRequest.GetResponse() } catch {}
+  $cert = $webRequest.ServicePoint.Certificate
+  Write-Verbose "Adding PullServer Root Certificate to Cert:\LocalMachine\Root"
+  Get-ChildItem -Path "Cert:\LocalMachine\Root\" | ? Subject -EQ $("CN=", $nodeinfo.PullServerName -join '') | Remove-Item
+  $store = Get-Item Cert:\LocalMachine\Root
+  $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]'ReadWrite')
+  $store.Add($cert.Export([Security.Cryptography.X509Certificates.X509ContentType]::Cert))
+  $store.Close()
+}
+
+Export-ModuleMember -Function *-TargetResource
